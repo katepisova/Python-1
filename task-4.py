ef4e72cb3b7a91cbebdf27711cfcaa0fff0a2fb2
katepisova <katepisova97@gmail.com>
2018-07-11 21:36:00 +0300

    Task 2

--- task-4.py
+++ task-4.py
@@ -2,8 +2,14 @@ from sys import *
 from cmath import *
 import numpy
 
-for n in argv[1:]:
-    if float(n) > 0:
-        print ("ln(%g)" % float(n), "=", log(float(n)))
+# for n in argv[1:]:
+#     if float(n) > 0:
+#         print("ln(%g)" % float(n), "=", log(float(n)))
+#     else:
+#         print("ln(%g)" % float(n), "is incorrect")
+
+for i in range(1, len(argv)):
+    if float(argv[i]) > 0:
+        print("ln(%g)" % float(argv[i]), "=", log(float(argv[i])))
     else:
-        print("ln(%g)" % float(n), "is incorrect")
+        print("ln(%g)" % float(argv[i]), "is incorrect")
