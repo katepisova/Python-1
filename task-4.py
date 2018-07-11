9f38fe0e554ea2214cbf6b371d99d569dfcd3555
katepisova <katepisova97@gmail.com>
2018-07-11 21:42:49 +0300

    Task 3

--- task-4.py
+++ task-4.py
@@ -4,12 +4,22 @@ import numpy
 
 # for n in argv[1:]:
 #     if float(n) > 0:
-#         print("ln(%g)" % float(n), "=", log(float(n)))
+#         print ("ln(%g)" % float(n), "=", log(float(n)))
 #     else:
 #         print("ln(%g)" % float(n), "is incorrect")
 
-for i in range(1, len(argv)):
-    if float(argv[i]) > 0:
+# for i in range(1, len(argv)):
+#     if float(argv[i]) > 0:
+#         print("ln(%g)" % float(argv[i]), "=", log(float(argv[i])))
+#     else:
+#         print("ln(%g)" % float(argv[i]), "is incorrect")
+
+i = 1;
+while i < len(argv):
+    if float(argv[i])>0:
         print("ln(%g)" % float(argv[i]), "=", log(float(argv[i])))
     else:
         print("ln(%g)" % float(argv[i]), "is incorrect")
+    i =  i + 1;
+
+
