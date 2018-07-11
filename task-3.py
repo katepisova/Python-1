e7f4ce2b843205ee6c0e362224c00a32d49f86be
katepisova <katepisova97@gmail.com>
2018-07-11 20:54:30 +0300

    Error 1
    Making correct the location of brackets in the line 9.

---
+++ task-3.py
@@ -0,0 +1,9 @@
+import sys, random
+def compute(n):
+    i = 0; s = 0
+    while i <= n:
+        s += random.random()
+        i += 1
+    return s/n
+n = sys.argv[1]
+print ('average of %d random numbers is %g' % (n, compute(n))) # error 1
